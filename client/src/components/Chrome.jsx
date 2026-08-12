1ebba86b64e370d12c365c9061e64dd5e3bb83b3
Gowtham-Gtm21 <gowthamgtm221@gmail.com>
2026-08-12 09:20:52 +0530

    VLT landing page

--- client/src/components/Chrome.jsx
+++ client/src/components/Chrome.jsx
@@ -3,7 +3,7 @@ import { m, useScroll, useSpring, useMotionValueEvent } from 'motion/react';
 import { Mail, Phone, Pin, LinkedIn, Instagram, WhatsApp } from './Icons';
 
 export const WHATSAPP_NUMBER = '9791670504';
-export const PHONE_DISPLAY = '+91 97916 70504';
+export const PHONE_DISPLAY = '+91 93422 16211';
 export const EMAIL = 'sutheesh.s@vulturelines.com';
 
 /**
